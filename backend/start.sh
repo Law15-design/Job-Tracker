a2f2ea3191466baf266068658f0d6ae551cb5cae
Law15-design <khanmuhammadmaaz2004@yahoo.com>
2026-08-05 23:44:26 -0500

    Simplify start.sh by removing comments

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# Render gives us a random port number in $PORT
-# Tomcat normally only listens on 8080, so we swap it here before starting
 sed -i "s/8080/${PORT:-10000}/g" /usr/local/tomcat/conf/server.xml
 exec catalina.sh run
